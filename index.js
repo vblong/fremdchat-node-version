dd5c85749d8d26e1d44911d2393aed26a9c72526
vblong <vu09@ads.uni-passau.de>
2021-04-07 23:17:09 +0200

    Use code from Stackoverflow

--- index.js
+++ index.js
@@ -1,36 +1,44 @@
-'use strict';
+// 'use strict';
 
-// Imports dependencies and set up http server
-const
-  express = require('express'),
-  bodyParser = require('body-parser'),
-  app = express().use(bodyParser.json()); // creates express http server
+// // Imports dependencies and set up http server
+// const
+//   express = require('express'),
+//   bodyParser = require('body-parser'),
+//   app = express().use(bodyParser.json()); // creates express http server
 
-// Sets server port and logs message on success
-app.listen(process.env.PORT || 3000, () => console.log('webhook is listening'));
+// // Sets server port and logs message on success
+// app.listen(process.env.PORT || 3000, () => console.log('webhook is listening'));
 
-// Creates the endpoint for our webhook 
-app.post('/webhook', (req, res) => {  
+// // Creates the endpoint for our webhook 
+// app.post('/webhook', (req, res) => {  
  
-  let body = req.body;
+//   let body = req.body;
 
-  // Checks this is an event from a page subscription
-  if (body.object === 'page') {
+//   // Checks this is an event from a page subscription
+//   if (body.object === 'page') {
 
-    // Iterates over each entry - there may be multiple if batched
-    body.entry.forEach(function(entry) {
+//     // Iterates over each entry - there may be multiple if batched
+//     body.entry.forEach(function(entry) {
 
-      // Gets the message. entry.messaging is an array, but 
-      // will only ever contain one message, so we get index 0
-      let webhook_event = entry.messaging[0];
-      console.log(webhook_event);
-    });
+//       // Gets the message. entry.messaging is an array, but 
+//       // will only ever contain one message, so we get index 0
+//       let webhook_event = entry.messaging[0];
+//       console.log(webhook_event);
+//     });
 
-    // Returns a '200 OK' response to all requests
-    res.status(200).send('EVENT_RECEIVED');
-  } else {
-    // Returns a '404 Not Found' if event is not from a page subscription
-    res.sendStatus(404);
-  }
+//     // Returns a '200 OK' response to all requests
+//     res.status(200).send('EVENT_RECEIVED');
+//   } else {
+//     // Returns a '404 Not Found' if event is not from a page subscription
+//     res.sendStatus(404);
+//   }
 
-});
+// });
+
+var http = require('http');
+
+http.createServer(function (req, res) {
+    res.writeHead(200, {'Content-Type': 'text/plain'});
+    res.end('Hello World\n');
+}).listen(3000, "0.0.0.0");
+console.log('Server running at http://0.0.0.0:3000/');
