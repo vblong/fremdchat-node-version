9f0a15f123f788531520fa90fb86ef27b597e701
Long Vu <53224731+vblong@users.noreply.github.com>
2021-04-23 03:00:15 +0200

    Base service for SSL

--- index.js
+++ index.js
@@ -1,44 +1,7 @@
-// 'use strict';
+const express = require('express');
+const app = express();
+const port = 3000;
 
-// // Imports dependencies and set up http server
-// const
-//   express = require('express'),
-//   bodyParser = require('body-parser'),
-//   app = express().use(bodyParser.json()); // creates express http server
+app.get('/', (req, res) => res.send('Hello World!'));
 
-// // Sets server port and logs message on success
-// app.listen(process.env.PORT || 3000, () => console.log('webhook is listening'));
-
-// // Creates the endpoint for our webhook 
-// app.post('/webhook', (req, res) => {  
- 
-//   let body = req.body;
-
-//   // Checks this is an event from a page subscription
-//   if (body.object === 'page') {
-
-//     // Iterates over each entry - there may be multiple if batched
-//     body.entry.forEach(function(entry) {
-
-//       // Gets the message. entry.messaging is an array, but 
-//       // will only ever contain one message, so we get index 0
-//       let webhook_event = entry.messaging[0];
-//       console.log(webhook_event);
-//     });
-
-//     // Returns a '200 OK' response to all requests
-//     res.status(200).send('EVENT_RECEIVED');
-//   } else {
-//     // Returns a '404 Not Found' if event is not from a page subscription
-//     res.sendStatus(404);
-//   }
-
-// });
-
-var http = require('https');
-
-http.createServer(function (req, res) {
-    res.writeHead(200, {'Content-Type': 'text/plain'});
-    res.end('Hello World\n');
-}).listen(3000, "0.0.0.0");
-console.log('Server running at http://0.0.0.0:3000/');
+app.listen(port, () => console.log(`Service running on port ${port}`));
