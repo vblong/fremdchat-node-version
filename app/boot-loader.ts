4d060aea32dafa28b695b95e0312b9c48a04b6b9
vblong <vu09@ads.uni-passau.de>
2021-04-27 20:08:21 +0200

    Update graph API V10.0

--- app/boot-loader.ts
+++ app/boot-loader.ts
@@ -167,7 +167,7 @@ export class BootLoader {
   
     // Send the HTTP request to the Messenger Platform
     request({
-      'uri': 'https://graph.facebook.com/v2.6/me/messages',
+      'uri': 'https://graph.facebook.com/v10.0/me/messages',
       'qs': { 'access_token': PAGE_ACCESS_TOKEN },
       'method': 'POST',
       'json': requestBody
