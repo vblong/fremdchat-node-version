1a5f60896b1e57f2d4f6cdda711b4233eb07a928
vblong <vu09@ads.uni-passau.de>
2021-04-30 00:13:18 +0200

    Add db-config.ts but with dummy text

---
+++ app/db/db-config.ts
@@ -0,0 +1,5 @@
+export const DB_HOST = "localhost";
+export const DB_USER = "<DB_USER>";
+export const DB_PASS = "<DB_PASSWORD>";
+export const DB_NAME = "<DB_NAME>";
+export const DB_COLLATION = "utf8mb4";
