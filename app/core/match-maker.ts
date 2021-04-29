025aceddc2dfdf1538cad070f892a0dd0b37ff7f
vblong <vu09@ads.uni-passau.de>
2021-04-30 00:57:55 +0200

    TODO - MatchMaker

---
+++ app/core/match-maker.ts
@@ -0,0 +1,7 @@
+export class MatchMaker {
+    constructor() {}
+
+    matching(userid: string, orientation: number) {
+        console.log(`Start matching for user ${userid}, orientation ${orientation}`);
+    }
+}
