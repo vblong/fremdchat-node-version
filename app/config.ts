4855522303b97bc2e4001ef727b48648b6c5e853
vblong <vu09@ads.uni-passau.de>
2021-04-30 00:16:13 +0200

    Remove access token from public

---
+++ app/config.ts
@@ -0,0 +1,4 @@
+export const GRAPH_API_VERSION = 'v10.0';
+
+export const USER_MIN_AGE_ALLOWABLE = 8;
+export const USER_MAX_AGE_ALLOWABLE = 120;
---
+++ app/environment/environment-dev.ts
@@ -0,0 +1,3 @@
+// The page access token we have generated in your app settings
+//  Token - Gia Re Bat Ngo
+export const PAGE_ACCESS_TOKEN = '<YOUR_FACEBOOK_APP_ACCESS_TOKEN';
